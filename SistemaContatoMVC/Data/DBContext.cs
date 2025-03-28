46637aab4a7bc44408e49bb986ab97e6fb09c6d9
huandres-schmidt <huandreschmidt@gmail.com>
2025-03-27 21:55:01 -0300

    feat(): Commit initial

---
+++ SistemaContatoMVC/Data/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaContatoMVC.Models;
+
+    public class DBContext : DbContext
+    {
+        public DBContext (DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaContatoMVC.Models.Contato> Contato { get; set; } = default!;
+    }
---
+++ SistemaContatoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaContatoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
